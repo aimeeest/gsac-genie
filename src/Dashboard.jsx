157eb7e3d6172bf041027ef9dd764b5b363d8bd9
Aimee <estabayaaimee@gmail.com>
2023-12-12 22:13:09 +0800

    tru

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
     <div>
       <img src={genie} alt="genie" className='genie' />
       <h2>Hi, I'm a GSAC Genie!</h2>
-      <p>A GSAC Member helper.</p>
+      <p>A GSAC Member helper..</p>
       <div className="nav">
       <div className="container1">
         <Link to="/announcements">What's new?</Link>
